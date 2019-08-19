3a15fd3fcb4cb54b177a89404705572c8a2de7ba
Darren Labithiotis <labithiotis@gmail.com>
2019-08-19 09:22:01 +0100

    check GCLOUD_AUTH is set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
-echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
-sh -c "gcloud auth activate-service-account --key-file=$HOME/gcloud.json"
+if [ -n "$GCLOUD_AUTH" ]; then
+  echo "Authenticating gcloud..."
+  echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
+  sh -c "gcloud auth activate-service-account --key-file=$HOME/gcloud.json"
+fi
